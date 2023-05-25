e858f3a1da520e6f40b49f76ff16577645d63ebd
Wagner <wprodriguesbr@gmail.com>
2023-05-25 12:01:00 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+hackbitmotors.Servo(hackbitmotors.Servos.S8, 100)
+basic.forever(function () {
+    if (input.lightLevel() > 10) {
+        hackbitmotors.Servo(hackbitmotors.Servos.S8, 140)
+        basic.pause(500)
+        hackbitmotors.Servo(hackbitmotors.Servos.S8, 100)
+        basic.pause(500)
+        hackbitmotors.Servo(hackbitmotors.Servos.S8, 60)
+        basic.pause(500)
+        hackbitmotors.Servo(hackbitmotors.Servos.S8, 100)
+        basic.pause(500)
+    }
+})
